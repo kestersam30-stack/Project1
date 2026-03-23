241a77a6bc7942a02a4e31f3729fc63bdb60ab94
Sam Kester <kestersam30@gmail.com>
2026-03-23 19:02:15 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-IMAGE_NAME=kesterr15/devops-app
 
-docker pull $IMAGE_NAME:latest
+IMAGE="kesterr15/devops-app-dev:latest"
+
+echo "Stopping old container..."
 docker stop app || true
+
+echo "Removing old container..."
 docker rm app || true
 
-docker run -d -p 80:80 --name app $IMAGE_NAME:latest
+echo "Running new container..."
+docker run -d -p 80:80 --name app $IMAGE
